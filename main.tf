e00383271852885b8a9a039f14ec3fd2f944c2d0
Ryan <ryan.fee625@gmail.com>
2020-01-24 12:48:39 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_efs_file_system" "scalr" {
   creation_token = var.efs_name
 }
 
-resource "aws_efs_mount_target" "alpha" {
+resource "aws_efs_mount_target" "scalr" {
   file_system_id = "${data.aws_efs_file_system.scalr.id}"
   subnet_id      = var.subnnet
   ip_address     = var.ip_address
