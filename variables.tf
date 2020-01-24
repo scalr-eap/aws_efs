305b8b330f19aae9f98c6bdcb31b5fbba41fbb41
Ryan <ryan.fee625@gmail.com>
2020-01-24 12:27:48 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  description = "region"
+}
+
+variable "efs_name" {
+  description = "Name of the EFS"
+}
+
+variable "subnnet" {
+  description = "region"
+}
+
+variable "subnnet" {
+  description = "ip_address"
+}
