17cb43924451212a0d0c5da5972b9c0167293ffe
Ryan <ryan.fee625@gmail.com>
2020-01-24 12:26:08 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region     = var.region
+}
+
+resource "aws_efs_file_system" "scalr" {
+  creation_token = var.efs_name
+}
+
+resource "aws_efs_mount_target" "alpha" {
+  file_system_id = "${data.aws_efs_file_system.scalr.id}"
+  subnet_id      = var.subnnet
+  ip_address     = var.ip_address
+}
