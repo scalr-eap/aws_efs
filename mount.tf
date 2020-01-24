abd9e778749c167fbcb15065d94514578ed39df4
Ryan <ryan.fee625@gmail.com>
2020-01-24 12:56:12 -0500

    Update mount.tf

--- mount.tf
+++ mount.tf
@@ -1,3 +1,7 @@
+data "aws_efs_file_system" "scalr" {
+  name = var.efs_name
+}
+
 resource "aws_efs_mount_target" "scalr" {
   file_system_id = "${data.aws_efs_file_system.scalr.id}"
   subnet_id      = var.subnnet
